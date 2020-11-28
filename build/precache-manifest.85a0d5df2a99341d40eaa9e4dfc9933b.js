0112b26a72034e8a2568f0c4550f75e16fb302b7
BashirSidi <bashirsalisusidi@gmail.com>
2020-11-28 10:06:56 +0100

    added a build dir

---
+++ build/precache-manifest.85a0d5df2a99341d40eaa9e4dfc9933b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9955fa77a1b79eb8c80cdfc0d22b338f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7cf6a84eb36f8bdde68",
+    "url": "/static/css/main.7d86f3ee.chunk.css"
+  },
+  {
+    "revision": "955141a057ac5ed98d3a",
+    "url": "/static/js/2.d5f620be.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d5f620be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7cf6a84eb36f8bdde68",
+    "url": "/static/js/main.378f3a9e.chunk.js"
+  },
+  {
+    "revision": "c469b5aedbcadcf4ce68",
+    "url": "/static/js/runtime-main.2846b622.js"
+  }
+]);
